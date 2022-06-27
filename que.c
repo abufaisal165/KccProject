f1266e2c0d2ba1f60415e3c464dd2bad6c7522b7
abufaisal165 <78465503+abufaisal165@users.noreply.github.com>
2022-06-28 03:37:56 +0530

    Can sdme check this code, whats wrong?

--- que.c
+++ que.c
@@ -1,1 +1,34 @@
-qwertyu
+#include<stdio.h>
+
+int main ()
+{
+    int arr[m][n];
+    int i,j;
+    printf("Enter the number of row");
+    scanf("%d",$m);
+
+    printf("Enter the number of column");
+    scanf("%d",$n);
+
+    for(i=0;i<m; i++);
+    {
+        for(j=0; j<n; j++);
+        {
+            printf("Enter the value of array[%d][%d]is", i,j);
+            scanf("%d", $ arr[i][j]);
+        }
+    }
+    printf("The element of two dimesion array is: ");
+    for(i=0; i<m; i++);
+    {
+        for(j=0; j<n; j++);
+        {
+            printf("%d", arr[i][j])
+            if(j==m)
+            {
+                printf("\n");
+            }
+        }
+    }
+    return 0;
+}
